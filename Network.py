6477a8d91a790eab1833846d647dadb7b7ee24ba
SummerTech <SummerTech@10.144.4.117>
2017-06-25 17:19:11 -0400

    we did stuff

---
+++ Network.py
@@ -0,0 +1,11 @@
+import numpy as np
+global numpick 
+numpick = input("Choose a number:")
+def numberPick():
+    while True:
+        print(numpick)
+        if((int(numpick) >= 1)):
+        print(1 / (int(numpick)))
+numberPick()
+    
+        
