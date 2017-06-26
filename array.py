82ed51ac39ac86a690438032bde676cc47b399a0
SummerTech <SummerTech@10.144.4.117>
2017-06-26 11:09:54 -0400

    We did stuff

--- array.py
+++ array.py
@@ -1,13 +1,26 @@
-shop = input("Do you want to add or take away from your shopping list. Say A for add or D for take away:")
-what = 'What do you want to take away:'
-No = 'What do you want to add:'
+
 bb = []
-if shop == 'A':
-    input(No)
-    bb.append(No)
-    print(bb)
-if shop == 'D':
-    print(what)
+while True:
+    global shop
+    shop = input("Do you want to add or take away from your shopping list. Say A for add or D for take away:")
+    if shop == 'A':
+        no = input('What do you want to add:')
+        if bb.count(no) == 0:
+            bb.append(no)
+            print(bb)
+        else:
+            print('You cant add more than once')
+            print(bb)
+    global what
+    if shop == 'D':
+        what = input('What do you want to take away:')
+        if bb.count(what) == 0:
+            print("You cant remove that because it is not in your list")
+        else:
+            bb.remove(what)
+            print(bb)
+        
+    
     
     
 
