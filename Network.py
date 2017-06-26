8c895d8ccd2d1aadff8e1cb7c07357ed1f056ea4
SummerTech <SummerTech@10.144.4.117>
2017-06-26 10:00:16 -0400

    We did stuff

--- Network.py
+++ Network.py
@@ -1,11 +1,30 @@
 import numpy as np
 global numpick 
 numpick = input("Choose a number:")
-def numberPick():
-    while True:
-        print(numpick)
-        if((int(numpick) >= 1)):
+def sigmoid():
+    print(numpick)
+    if((int(numpick) > 1)):
         print(1 / (int(numpick)))
-numberPick()
+    else:
+        print(numpick)
+sigmoid()
+
+def node(i):
+    for i in range:
+        [0][2]
+        [0][3]
+        [0][4]
+        [1][2]
+        [1][3]
+        [1][4]
+        [2][5]
+        [3][5]
+        [4][5]
+        
+        
+    
+        
+
+
     
         
---
+++ array.py
@@ -0,0 +1,20 @@
+shop = input("Do you want to add or take away from your shopping list. Say A for add or D for take away:")
+what = 'What do you want to take away:'
+No = 'What do you want to add:'
+bb = []
+if shop == 'A':
+    input(No)
+    bb.append(No)
+    print(bb)
+if shop == 'D':
+    print(what)
+    
+    
+
+   
+  
+    
+        
+         
+  
+    
