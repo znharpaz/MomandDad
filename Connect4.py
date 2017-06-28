f7cb9d1cdab254cc089ff040ae1989bd96b92acd
SummerTech <SummerTech@10.144.4.117>
2017-06-28 11:14:28 -0400

    hi

--- Connect4.py
+++ Connect4.py
@@ -6,19 +6,57 @@ for i in aa:
 
         
 while True:
-    a = int(input('Pick a column.'))
-    aa[5][a] = 'x'
-    for i in range(5,0,-1):
-        if aa[i][a] == 'x':
-            break
-        elif aa[i][a] == '-':
+    a = int(input('Player one pick a column.'))
+    for i in range(5,-1,-1):
+        if aa[i][a] == '-':
             aa[i][a] = 'x'
-        
+            break
+        elif aa[i][a] == 'x':
+            pass
+        if aa[0][a] != '-':
+            i = 5
+            a = int(input('Player one pick a column.'))
+            
         
     for r in range(0,6):
         print() 
         for c in range(0,7):
             print(aa[r][c],end = '')
+    
+    for i in range(0,3):
+        for h in range(0,7):
+            if aa[i][h] == 'x' and aa[i+1][h] == 'x' and aa[i+2][h] == 'x' and aa[i+3][h] == 'x':
+                print('You Win!')
+    
+                
+    
+    
+    
+    
+    a = int(input('Player two pick a column.'))
+    for i in range(5,-1,-1):
+        if aa[i][a] == '-':
+            aa[i][a] = 'o'
+            break
+        elif aa[i][a] == 'o':
+            pass
+        if aa[0][a] != '-':
+            i = 5
+            a = int(input('Player two pick a column.'))
+    for r in range(0,6):
+        print() 
+        for c in range(0,7):
+            print(aa[r][c],end = '')
+            
+            
+    for i in range(0,3):
+        for h in range(0,7):
+            if aa[i][h] == 'o' and aa[i+1][h] == 'o' and aa[i+2][h] == 'o' and aa[i+3][h] == 'o':
+                print('You Win!')
+                break
+           
+        
+   
     
     
     
