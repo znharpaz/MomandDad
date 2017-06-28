336e6610a10c45af12acf38ae5b9ca8ef43b4d7b
SummerTech <SummerTech@10.144.4.117>
2017-06-28 13:33:04 -0400

    hi

--- Connect4.py
+++ Connect4.py
@@ -28,8 +28,17 @@ while True:
             if aa[i][h] == 'x' and aa[i+1][h] == 'x' and aa[i+2][h] == 'x' and aa[i+3][h] == 'x':
                 print('You Win!')
     
-                
+    for i in range(0,4):
+        for h in range(0,6):
+            if aa[i][h] == 'x' and aa[i][h+1] == 'x' and aa[i][h+2] == 'x' and aa[i][h+3] == 'x' and aa[i][h+4] == 'x':
+                print('You Win!')
     
+                
+    for i in range(0,2):
+        for h in range(0,3):
+            if aa[i][h] == 'x' and aa[i+1][h+1] == 'x' and aa[i+2][h+2] == 'x' and aa[i+3][h+3] == 'x':
+                print('You Win!')
+                break
     
     
     
@@ -54,6 +63,12 @@ while True:
             if aa[i][h] == 'o' and aa[i+1][h] == 'o' and aa[i+2][h] == 'o' and aa[i+3][h] == 'o':
                 print('You Win!')
                 break
+            
+            
+    for i in range(0,4):
+        for h in range(0,6):
+            if aa[i][h] == 'o' and aa[i][h+1] == 'o' and aa[i][h+2] == 'o' and aa[i][h+3] == 'o' and aa[i][h+4] == 'o':
+                print('You Win!')
            
         
    
