68c287eed102f516aa07ae237ce8cb46a32911c7
SummerTech <SummerTech@10.144.4.117>
2017-06-28 10:01:26 -0400

    hi hi

--- Connect4.py
+++ Connect4.py
@@ -1,19 +1,27 @@
-a = int(input('Pick a column.'))
 aa=[[],[],[],[],[],[]]
 for i in aa:
     for x in range(0,7):
         i.append('-')
-print(aa)
-for r in range(0,6):
-    print() 
-    for c in range(0,7):
-        print(aa[r][c],end = '')
+        
+
+        
 while True:
-    for a in range(5,0,-1):
-        if a == '-':
-            c =+ 1
-        else:
-            aa[r][c] = 'x'
+    a = int(input('Pick a column.'))
+    aa[5][a] = 'x'
+    for i in range(5,0,-1):
+        if aa[i][a] == 'x':
+            break
+        elif aa[i][a] == '-':
+            aa[i][a] = 'x'
+        
+        
+    for r in range(0,6):
+        print() 
+        for c in range(0,7):
+            print(aa[r][c],end = '')
+    
+    
+    
     
            
 
