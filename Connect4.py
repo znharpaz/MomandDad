99ab0ed6476d3b31b6353bf77abfd6965cbb5e41
SummerTech <SummerTech@10.144.4.117>
2017-06-29 10:00:32 -0400

    Hi

--- Connect4.py
+++ Connect4.py
@@ -2,10 +2,11 @@ aa=[[],[],[],[],[],[]]
 for i in aa:
     for x in range(0,7):
         i.append('-')
+dd = 1
         
 
         
-while True:
+while dd == 1:
     a = int(input('Player one pick a column.'))
     for i in range(5,-1,-1):
         if aa[i][a] == '-':
@@ -26,21 +27,31 @@ while True:
     for i in range(0,3):
         for h in range(0,7):
             if aa[i][h] == 'x' and aa[i+1][h] == 'x' and aa[i+2][h] == 'x' and aa[i+3][h] == 'x':
-                print('You Win!')
+                print('     You Win!')
+                dd = 0
     
-    for i in range(0,4):
-        for h in range(0,6):
-            if aa[i][h] == 'x' and aa[i][h+1] == 'x' and aa[i][h+2] == 'x' and aa[i][h+3] == 'x' and aa[i][h+4] == 'x':
-                print('You Win!')
+    for i in range(0,6):
+        for h in range(0,4):
+            if aa[i][h] == 'x' and aa[i][h+1] == 'x' and aa[i][h+2] == 'x' and aa[i][h+3] == 'x':
+                print('     You Win!')
+                dd = 0
     
                 
-    for i in range(0,2):
-        for h in range(0,3):
+    for i in range(0,3):
+        for h in range(0,4):
             if aa[i][h] == 'x' and aa[i+1][h+1] == 'x' and aa[i+2][h+2] == 'x' and aa[i+3][h+3] == 'x':
-                print('You Win!')
-                break
+                print('     You Win!')
+                dd = 0
+    for i in range(0,3):
+        for h in range(0,7):
+            if aa[i][h] == 'x' and aa[i+1][h-1] == 'x' and aa[i+2][h-2] == 'x' and aa[i+3][h-3] == 'x':
+                print('     You Win!')
+                dd = 0  
     
-    
+   
+    if dd == 0:
+        break
+   
     
     a = int(input('Player two pick a column.'))
     for i in range(5,-1,-1):
@@ -61,16 +72,26 @@ while True:
     for i in range(0,3):
         for h in range(0,7):
             if aa[i][h] == 'o' and aa[i+1][h] == 'o' and aa[i+2][h] == 'o' and aa[i+3][h] == 'o':
-                print('You Win!')
-                break
-            
-            
-    for i in range(0,4):
-        for h in range(0,6):
-            if aa[i][h] == 'o' and aa[i][h+1] == 'o' and aa[i][h+2] == 'o' and aa[i][h+3] == 'o' and aa[i][h+4] == 'o':
-                print('You Win!')
-           
-        
+                print('     You Win!')
+                dd = 0
+    
+    for i in range(0,6):
+        for h in range(0,4):
+            if aa[i][h] == 'o' and aa[i][h+1] == 'o' and aa[i][h+2] == 'o' and aa[i][h+3] == 'o':
+                print('     You Win!')
+                dd = 0
+    
+                
+    for i in range(0,3):
+        for h in range(0,4):
+            if aa[i][h] == 'o' and aa[i+1][h+1] == 'o' and aa[i+2][h+2] == 'o' and aa[i+3][h+3] == 'o':
+                print('     You Win!')
+                dd = 0
+    for i in range(0,3):
+        for h in range(0,7):
+            if aa[i][h] == 'o' and aa[i+1][h-1] == 'o' and aa[i+2][h-2] == 'o' and aa[i+3][h-3] == 'o':
+                print('     You Win!')
+                dd = 0  
    
     
     
